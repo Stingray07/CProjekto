03322fc2db61d1d43bd7429eeb3bb919373e4f5b
Stingray <stingraysonggay@gmail.com>
2025-07-10 18:44:44 +0800

    Project initializationnnnn

---
+++ MyApiProject/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.MapPost("/postyear", ([FromBody] string year) => 
+{
+    return "Year: " + year;
+}).WithName("PostYear");
+
+app.MapGet("/hello", () => "Hello World!")
+.WithName("HelloWorld");
+
+app.MapGet("/hello/{name}", (string name) => "Hello " + name)
+.WithName("HelloName");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
